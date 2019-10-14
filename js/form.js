ca38ff7c4986ac7a2d2cf4e6980ae3d2b6028a2b
Julia <ggfox@mail.ru>
2019-10-14 15:57:53 +0300

    Синхронизация

--- js/form.js
+++ js/form.js
@@ -2,11 +2,44 @@
 
 
 (function () {
-
   var adForm = document.querySelector('.ad-form');
+  var typeRent = adForm.querySelector('#type');
+  var priceNight = adForm.querySelector('#price');
   var roomNumberSelect = adForm.querySelector('#room_number');
   var capacitySelect = adForm.querySelector('#capacity');
   var adFormReset = adForm.querySelector('.ad-form__reset');
+  var timeOut = adForm.querySelector('#timeout');
+  var timeIn = adForm.querySelector('#timein');
+
+  timeOut.addEventListener('change', function () {
+    timeIn.value = timeOut.value;
+  });
+  timeIn.addEventListener('change', function () {
+    timeOut.value = timeIn.value;
+  });
+  function onPriceChange() {
+    var typeValue = typeRent.value;
+    if (typeValue === 'bungalo') {
+      priceNight.placeholder = '0';
+      priceNight.min = '0';
+    } else if (typeValue === 'flat') {
+      priceNight.placeholder = '1000';
+      priceNight.min = '1000';
+    } else if (typeValue === 'house') {
+      priceNight.placeholder = '5000';
+      priceNight.min = '5000';
+    } else if (typeValue === 'palace') {
+      priceNight.placeholder = '10000';
+      priceNight.min = '10000';
+    }
+  }
+  typeRent.addEventListener('change', onPriceChange);
+  roomNumberSelect.addEventListener('change', function () {
+    capacitySelect.value = roomNumberSelect.value;
+  });
+  capacitySelect.addEventListener('change', function () {
+    roomNumberSelect.value = capacitySelect.value;
+  });
 
   function onFormInput() {
     var roomNumber = roomNumberSelect.value;
@@ -23,15 +56,10 @@
   roomNumberSelect.addEventListener('change', onFormInput);
 
   function selectType() {
-    var typeRent = adForm.querySelector('#type');
-    var priceNight = adForm.querySelector('#price');
-    var priceValue = priceNight.value;
-    var typeValue = typeRent.value;
-    var timeOut = adForm.querySelector('#timeout');
-    var timeIn = adForm.querySelector('#timein');
     var timeOutValue = timeOut.value;
     var timeInValue = timeIn.value;
-
+    var typeValue = typeRent.value;
+    var priceValue = priceNight.value;
     if (typeValue === 'bungalo' && priceValue < 0) {
       priceNight.setCustomValidity('Цена должна быть выше');
     } else if (typeValue === 'flat' && priceValue < 1000) {
